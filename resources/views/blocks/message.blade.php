034e00a2edeb2844f67d86d3d5e0fcb9ebfa8f59
ktago336 <stas.sutulo.work@gmail.com>
2024-05-10 22:51:21 +0300

    messages avatar fixed

--- resources/views/blocks/message.blade.php
+++ resources/views/blocks/message.blade.php
@@ -1,6 +1,6 @@
 @if($message->from_id == $userId)
     <div class="my-message d-flex justify-content-end">
-        <img class="author-image" src="{{$message->to->avatar()->url}}" alt="Author Image">
+        <img class="author-image" src="{{$message->from->avatar()->url}}" alt="Author Image">
         <div class="post-content">
             <p class="text-break"><b>Вы: </b>{{$message->text??''}}</p>
             @include('blocks.gallery',['images'=>$message->images])
